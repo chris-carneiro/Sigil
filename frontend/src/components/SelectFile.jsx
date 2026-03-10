235fc0334f01f30d2010ec984073c1ca7d082dbd
Chris <chris-carneiro@users.noreply.github.com>
2026-03-10 16:19:03 +0100

    feat(front): support file encryption before upload - add OnFileSelected function - add handleFile in app component to handle encryption

--- frontend/src/components/SelectFile.jsx
+++ frontend/src/components/SelectFile.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
-function SelectFile() {
+
+function SelectFile({ onFileSelected }) {
     const [file, setFile] = useState(null);
 
 
     return (
         <div>
-             <input type="file" onChange={e => setFile(e.target.files[0])} ></input>
+            <input type="file" onChange={e => setFile(e.target.files[0])} />
+            <input type="button" onClick={() => onFileSelected(file)} value="Upload" />
         </div>
     );
 }
