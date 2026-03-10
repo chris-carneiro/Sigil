b9868728b276f3be22457cd20e7152d93e73dbbd
Chris <chris-carneiro@users.noreply.github.com>
2026-03-10 16:19:03 +0100

    feat(front): support file encryption before upload - add OnFileSelected function - add handleFile in app component to handle encryption

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import SelectFile from './components/SelectFile'
+import encrypt from './secure/encryption';
 
 function App() {
   return (
     <>
-      <SelectFile />
+      <SelectFile onFileSelected={handleFile} />
     </>
   )
+}
+
+async function handleFile(file) {
+  console.log(file.name);
+  const {cipherText, rawKey, iv} = await encrypt(file);
+  console.log("cipher", new Uint8Array(cipherText).toBase64({ alphabet: "base64url" }));
 }
 
 export default App
