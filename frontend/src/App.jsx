9844017d091201b9dd4c4ff4abde599be9f3665b
Chris <chris-carneiro@users.noreply.github.com>
2026-03-12 16:11:28 +0100

    feat: generate QR code that encodes the URL to the encrypted file. - add dependency to react-qr-code - strip base64url padding for clean URL fragment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import './App.css'
 import SelectFile from './components/SelectFile'
 import encrypt from './secure/encryption';
+import QRCode from 'react-qr-code';
+
 
 function App() {
+  const [qrCodeUrl, setQRCodeUrl] = useState(null);
+
+  async function handleFile(file) {
+    const { cipherText, rawKey, iv } = await encrypt(file);
+
+    const formData = new FormData();
+    formData.append("document", new Blob([cipherText]));
+
+
+    const response = await fetch('/api/v1/documents', {
+      method: 'POST',
+      body: formData
+    });
+
+    const body = await response.json()
+    const encodedKey = new Uint8Array(rawKey).toBase64({ alphabet: "base64url", omitPadding: true });
+    const qrUrl = `${window.location.origin}/api/v1/documents/${body.documentId}#${encodedKey}`;
+
+    setQRCodeUrl(qrUrl);
+
+  }
+  if (qrCodeUrl) return <QRCode value={qrCodeUrl} />
+
   return (
     <>
       <SelectFile onFileSelected={handleFile} />
@@ -10,21 +36,6 @@ function App() {
   )
 }
 
-async function handleFile(file) {
-  console.log(file.name);
-  const {cipherText, rawKey, iv} = await encrypt(file);
 
-  const formData = new FormData();
-  formData.append("document", new Blob([cipherText]));
-
-
-  const response = await fetch('/api/v1/documents', {
-    method: 'POST',
-    body: formData
-  });
-
-  const body = await response.json()
-  console.log("Response body=", body.documentId);
-}
 
 export default App
