6a38ad111b3eda8f1b34ed8f9236dcac7177ac8e
Chris <chris-carneiro@users.noreply.github.com>
2026-03-12 17:30:35 +0100

    feat: add logo to the center of the QRCode. - change qrcode library to suppport image import - import sigil_mark.svg - add basic error handling - cleanup App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import './App.css'
 import SelectFile from './components/SelectFile'
 import encrypt from './secure/encryption';
-import QRCode from 'react-qr-code';
+import { QRCodeSVG } from 'qrcode.react';
+import sigilLogo from './assets/sigil_mark.svg';
+
+
 
 
 function App() {
   const [qrCodeUrl, setQRCodeUrl] = useState(null);
+  const [hasError, setError] = useState(null);
 
   async function handleFile(file) {
     const { cipherText, rawKey, iv } = await encrypt(file);
@@ -15,22 +19,51 @@ function App() {
     formData.append("document", new Blob([cipherText]));
 
 
-    const response = await fetch('/api/v1/documents', {
-      method: 'POST',
-      body: formData
-    });
+    try {
+      const response = await fetch('/api/v1/documents', {
+        method: 'POST',
+        body: formData
+      });
 
-    const body = await response.json()
-    const encodedKey = new Uint8Array(rawKey).toBase64({ alphabet: "base64url", omitPadding: true });
-    const qrUrl = `${window.location.origin}/api/v1/documents/${body.documentId}#${encodedKey}`;
+      if (!response.ok) {
+        throw Error("The encrypted document could not be stored")
+      }
 
-    setQRCodeUrl(qrUrl);
+      const body = await response.json()
+      const encodedKey = new Uint8Array(rawKey).toBase64({ alphabet: "base64url", omitPadding: true });
+      const qrUrl = `${window.location.origin}/api/v1/documents/${body.documentId}#${encodedKey}`;
+
+      setQRCodeUrl(qrUrl);
+    } catch (e) {
+      if (e instanceof TypeError) {
+        setError("The server could not be reached, check your connectivity")
+        return;
+      }
+      setError(e.message)
+    }
+
 
   }
-  if (qrCodeUrl) return <QRCode value={qrCodeUrl} />
+
+  if (qrCodeUrl) return <QRCodeSVG
+    value={qrCodeUrl}
+    title="Scan to download private document"
+    size={256}
+    bgColor={"#ffffff"}
+    fgColor={"#000000"}
+    level={"H"}
+    imageSettings={{
+      src: sigilLogo,
+      height: 56,
+      width: 56,
+      opacity: 1,
+      excavate: true,
+    }}
+  />
 
   return (
     <>
+      {hasError && <p className="error">{hasError}</p>}
       <SelectFile onFileSelected={handleFile} />
     </>
   )
