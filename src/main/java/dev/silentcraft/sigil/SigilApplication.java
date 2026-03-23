645bd0af82a16bd012646d99e35e1f07446a3931
Chris <chris-carneiro@users.noreply.github.com>
2026-03-23 18:32:47 +0100

    chore(infra): exclude RedisRepositoriesAutoConfiguration to suppress startup warning
    - Spring Data Redis scans for repositories at startup when RedisTemplate is on the classpath, producing a noisy warning for DocumentRepository
    - Exclude RedisRepositoriesAutoConfiguration since Redis repositories are not used
    - Document consequence in ADR-0003

--- src/main/java/dev/silentcraft/sigil/SigilApplication.java
+++ src/main/java/dev/silentcraft/sigil/SigilApplication.java
@@ -2,8 +2,9 @@ package dev.silentcraft.sigil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 public class SigilApplication {
 
 	public static void main(String[] args) {
