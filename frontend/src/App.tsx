740598d6040a4f500c3588bb076aba8df222fdbb
Chris <chris-carneiro@users.noreply.github.com>
2026-05-19 15:45:52 +0200

    feat(frontend): Add notFound Page for unhandle routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Main from './components/layout/Main';
 import UploadPage from './components/upload/UploadPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DownloadPage from './components/download/DownloadPage';
+import { NotFound } from './components/common/NotFound';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<UploadPage />} />
             <Route path="/documents/download/:id" element={<DownloadPage />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Main >
       </BrowserRouter>
---
+++ frontend/src/components/common/NotFound.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+import { Card } from './Card';
+import { Button } from './Button';
+import styles from './NotFound.module.css';
+
+export function NotFound() {
+    const navigate = useNavigate();
+    return (
+        <Card className={styles.card}>
+            <p className={styles.message}>It's okay to be lost sometimes...</p>
+            <Button label="Go Home" onClick={() => navigate('/')} />
+        </Card>
+    )
+}
