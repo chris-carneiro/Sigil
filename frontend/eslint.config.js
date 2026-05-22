7098599e49de90b412ea2cb4459faec6ed090ccd
Chris <chris-carneiro@users.noreply.github.com>
2026-05-22 23:25:45 +0200

    chore: fix eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -15,6 +15,7 @@ export default defineConfig([
             reactRefresh.configs.vite,
         ],
         languageOptions: {
+            parser: tseslint,
             ecmaVersion: 2020,
             globals: globals.browser,
             parserOptions: {
